9cf481293681a2a3d7608ae55186c515f1a1252b
mauricioyepez87 <89558474+mauricioyepez87@users.noreply.github.com>
2021-08-26 22:00:23 -0500

    Create pagina2.php

---
+++ pagina2.php
@@ -0,0 +1,9 @@
+<?php include("sesion.php"); ?>
+Bienvenido:
+<?php echo $_SESSION["usuario"]; ?>
+<br /><br />
+Estas en la seguna pagina PHP
+<br /><br />
+<a href="pagina1.php">Ir a la primera pagina segura</a>
+<br /><br />
+<a href="salir.php">SALIR</a>
